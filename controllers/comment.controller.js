b707f55e5eb83bb9338025c6a4c3ee4a0881e66b
asepridwan1989 <aseprmail@gmail.com>
2018-05-23 22:07:55 +0700

    add model

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,31 +1,32 @@
 const jwt = require('jsonwebtoken')
-const Article = require('../models/article.model')
+const Comment = require('../models/comment.model')
 const mongoose = require('mongoose')
 
 module.exports = {
   addComment: (req, res)=>{
     console.log(req.body)
-      // const token = req.headers.token
-      // let verified = jwt.decode(token,process.env.TOKENKEY)
-      // const userId = verified.id
-      // const title = req.body.title
-      // const content = req.body.content
-      // let newArticle = new Article({
-      //     userId,
-      //     title,
-      //     content
-      // })
-      // newArticle.save()
-      //     .then(result=>{
-      //         res.status(201).json({
-      //             message: 'successfuly add new article',
-      //             data: result
-      //         })
-      //     })
-      //     .catch(error=>{
-      //         res.status(400).json({
-      //             message: 'failed to add new task'
-      //         })
-      //     })
+      const token = req.headers.token
+      let verified = jwt.decode(token,process.env.TOKENKEY)
+      const userId = verified.id
+      const articleId = req.body.articleId
+      const content = req.body.content
+      let newComment = new Comment({
+          userId,
+          articleId,
+          content
+      })
+      newComment.save()
+        .then(result=>{
+          console.log('suksessssssss')
+            res.status(201).json({
+                message: 'successfuly add new article',
+                data: result
+            })
+        })
+        .catch(error=>{
+            res.status(400).json({
+                message: 'failed to add new task'
+            })
+        })
   }
 }
