3da1325d3998ad188b6ea4eb1279d0809b831767
asepridwan1989 <aseprmail@gmail.com>
2018-05-23 21:39:26 +0700

    add model

--- models/comment.js
+++ models/comment.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const articleSchema = new Schema({
+const commentSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
     ref: "user"
   },
+  articleId: {
+    type: Schema.Types.ObjectId,
+    ref: "article"
+  },
   liked: {
     type: Number,
-    default: 0
-  },
-  title: {
-    type: String,
-    require: [true, 'title required']
+    default: 25
   },
   content: {
     type: String,
